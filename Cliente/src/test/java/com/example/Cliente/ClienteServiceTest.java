cf9e8c5993e58784c6643ba9b89f5f4de1254f5d
matikkkkk <matydiscord2@gmail.com>
2026-06-16 10:29:40 -0400

    testcliente listo

---
+++ Cliente/src/test/java/com/example/Cliente/ClienteServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.Cliente;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import com.example.Cliente.Model.Cliente;
+import com.example.Cliente.Repository.ClienteRepository;
+import com.example.Cliente.Service.ClienteService;
+import java.util.Optional;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+@SpringBootTest
+public class ClienteServiceTest {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @MockBean
+    private ClienteRepository clienteRepository;
+
+    @Test
+    public void testListar() {
+        Cliente cliente = new Cliente(); 
+        when(clienteRepository.findAll()).thenReturn(List.of(cliente));
+        // Llama al método listar() del servicio.
+        List<Cliente> clientes = clienteService.listar();
+
+        // Verifica que la lista devuelta no sea nula y contenga exactamente un Cliente.
+        assertNotNull(clientes);
+        assertEquals(1, clientes.size());
+    }
+
+    @Test
+    public void testGuardar() {
+        Cliente clienteInput = new Cliente();
+        
+        when(clienteRepository.save(clienteInput)).thenReturn(clienteInput);
+
+        // Llama al método guardar() del servicio.
+        Cliente saved = clienteService.guardar(clienteInput);
+
+        // Verifica que el Cliente guardado no sea nulo.
+        assertNotNull(saved);
+        verify(clienteRepository, times(1)).save(clienteInput);
+    }
+
+    @Test
+    public void testBuscarPorId() {
+        Long id = 1L;
+        Cliente cliente = new Cliente();
+        // Define el comportamiento del mock: cuando se llame a findById() con el ID, devuelve un Cliente opcional.
+        when(clienteRepository.findById(id)).thenReturn(Optional.of(cliente));
+
+        // Llama al método buscarPorId() del servicio.
+        Cliente found = clienteService.buscarPorId(id);
+
+        // Verifica que el Cliente devuelto no sea nulo.
+        assertNotNull(found);
+        verify(clienteRepository, times(1)).findById(id);
+    }
+}
+
